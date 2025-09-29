46ab1dde668c9773a1bd6ce27df98680c1ff9f97
Damien <maitredede@users.noreply.github.com>
2025-09-29 14:03:03 +1100

    feat(test): added assertions

--- purego/version_test.go
+++ purego/version_test.go
@@ -1,9 +1,18 @@
 package plutopure
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestVersion(t *testing.T) {
-	t.Logf("version num: %d", VersionNumber())
+
+	minVersion := 0*10000 + 9*100 + 0
+
+	v := VersionNumber()
+	t.Logf("version num: %d", v)
+	assert.GreaterOrEqual(t, minVersion, v)
 }
 
 func TestVersionString(t *testing.T) {
